72b13763c443b0652df91fc29ef04e35d3a29b1b
antobbo <antobbo@gmail.com>
2017-09-01 17:24:47 +0100

    Added basic form module with visual toggling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { BookFilterPipe } from './books/book-filter.pipe';
+import { BookFormComponent } from './book-form/book-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent, BookFilterPipe, BookFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+      ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
